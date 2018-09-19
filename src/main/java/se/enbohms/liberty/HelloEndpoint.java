2ac06a8c324f900f98a18fc1dc501e6851d6c347
ae860468 <Andreas.Enbohm@telenor.se>
2018-09-19 13:07:58 +0200

    Initial commit

---
+++ src/main/java/se/enbohms/liberty/HelloEndpoint.java
@@ -0,0 +1,33 @@
+package se.enbohms.liberty;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.json.Json;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.microprofile.metrics.annotation.Counted;
+
+/**
+ * Represent a simple endpoint which just returns a JSON structure taking the
+ * path parameter as input (e.g. http://.../helidon/hello/john -> {"Hello " :
+ * "John"})
+ * 
+ * @author enbohm
+ *
+ */
+@ApplicationScoped
+@Path("/")
+public class HelloEndpoint {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/hello/{name}")
+	@Counted(monotonic = true)
+	public Response sayHi(@PathParam("name") String name) {
+		return Response.ok(Json.createObjectBuilder().add("Hello ", name).build()).build();
+	}
+}
---
+++ src/main/java/se/enbohms/liberty/MyApplication.java
@@ -0,0 +1,9 @@
+package se.enbohms.liberty;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MyApplication extends Application {
+
+}
