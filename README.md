413928338f5704c0e12716a5fab9a0a5463ad915
enbohm <andreas@enbohms.se>
2018-09-23 09:02:06 +0200

    Added circuit breaker showcase

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # liberty-mp
-A sample project using IBM OpenLiberty along with Microprofile 1.3 To run you need to have at least Java 8 and maven installed (type ''mvn clean install'' in the base dir to compile&package the application). To run, just type 'java -jar target/liberty-microprofile.jar'. To try it out, open http://127.0.0.1:8080/liberty/hello/you_name_goes_here . 
-The microprofile metrics can be found on https://127.0.0.1:8080/metrics (use admin/adminpwd to access the protected url)
+A sample project using IBM OpenLiberty along with Microprofile 1.4 To run you need to have at least Java 8 and maven installed (type ''mvn clean install'' in the base dir to compile&package the application). To run, just type 'java -jar target/liberty-mp.jar'. To try it out, open http://127.0.0.1:8080/liberty/hello/{your_name_goes_here} . 
+
+This endpoint includes counting of the total number of request (MP Metrics) and a circuit breaker (MP Fault Tolerance). To emulate runtime failures and to demonstrate the circuit breaker, exceptions are randomly thrown.
+
+The MP metrics can be viewed on http://localhost:8080/metrics and http://localhost:8080/metrics/application
--- src/main/java/se/enbohms/liberty/HelloEndpoint.java
+++ src/main/java/se/enbohms/liberty/HelloEndpoint.java
@@ -9,13 +9,22 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 
 /**
  * Represent a simple endpoint which just returns a JSON structure taking the
  * path parameter as input (e.g. http://.../helidon/hello/john -> {"Hello " :
  * "John"})
- * 
+ * <p>
+ * This endpoint includes counting of the total number of request (MP
+ * Metrics) and a circuit breaker (MP Fault Tolerance). To emulate runtime
+ * failures and to demonstrate the circuit breaker, exceptions are randomly
+ * thrown in the method below.
+ * <p>
+ * The metrics can be viewed on {@link http://localhost:8080/metrics} and
+ * {@link http://localhost:8080/metrics/application}
+ *
  * @author enbohm
  *
  */
@@ -27,7 +36,10 @@ public class HelloEndpoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/hello/{name}")
 	@Counted(monotonic = true)
+	@CircuitBreaker(failureRatio=0.5, requestVolumeThreshold=2)
 	public Response sayHi(@PathParam("name") String name) {
+		if (Math.random() > 0.75) //emulate 25% failures
+			throw new RuntimeException("An exception occured - this is to trigger the circuit breaker!");
 		return Response.ok(Json.createObjectBuilder().add("Hello ", name).build()).build();
 	}
 }
